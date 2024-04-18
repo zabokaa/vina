7f6e03d40bdbe754ea96c2dd831e7f52feca311b
zabokaa <sab_fem@gmx.de>
2024-04-18 06:19:01 +0200

    prep for deployment

--- vinopro/settings.py
+++ vinopro/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5#5i-iei7+tw$@0z!+c*+3c@e4%x)+xyvbreu5#d@&x!j*3=mk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".herokuapp.com"
+]
 
 
 # Application definition
