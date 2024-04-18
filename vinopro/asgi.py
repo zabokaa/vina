53c96439008efbffcc445c045a465c30c22da175
zabokaa <sab_fem@gmx.de>
2024-04-18 05:24:49 +0200

    create apps intor, blog, vinoteka + add to installed_apps

--- vinopro/asgi.py
+++ vinopro/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vinapro project.
+ASGI config for vinopro project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vinapro.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vinopro.settings")
 
 application = get_asgi_application()
