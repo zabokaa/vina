8923473cc42bed12120009600b1b35f8cd3944fb
zabokaa <sab_fem@gmx.de>
2024-04-19 18:26:59 +0200

    add docstrings to models @ intro/views

--- intro/views.py
+++ intro/views.py
@@ -4,9 +4,11 @@ from django.http import JsonResponse
 
 # Create your views here.
 def age_form(request):
+    """displaying the age verification form"""
     return render(request, 'intro/age_verification.html')
 
 def age_verification(request):
+    """checking if user is adult: only 18+years old can enter the page"""
     age = request.GET.get('age')
     if age is not None:
         is_adult = int(age) >= 18
