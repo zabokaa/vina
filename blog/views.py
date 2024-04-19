744a283e0b27fb31f7c728d042f0053713e82524
zabokaa <sab_fem@gmx.de>
2024-04-19 21:24:35 +0200

    add filter to PostList: only published posts

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,6 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
     paginate_by = 9
