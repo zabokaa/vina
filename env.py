6e3e147fa2a7a87f44e1d43081a5b138072c9088
zabokaa <sab_fem@gmx.de>
2024-04-19 16:28:41 +0200

    migrate after DB connect

--- env.py
+++ env.py
@@ -1,3 +1,6 @@
 import os
 
-os.environ.setdefault("DATABASE_URL", " postgres://usosuytnklm:rYMuG0PUYEVB@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/five_salon_flock_826794")
+os.environ.setdefault(
+    "DATABASE_URL", 
+    "postgres://usosuytnklm:rYMuG0PUYEVB@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/five_salon_flock_826794"
+    )
--- vinopro/settings.py
+++ vinopro/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
-    import env
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,11 +84,15 @@ WSGI_APPLICATION = "vinopro.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
