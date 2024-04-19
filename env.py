1a7200dbf7a3cdf2b0a83ed6d554dc4f5acad422
zabokaa <sab_fem@gmx.de>
2024-04-19 16:52:41 +0200

    change to different DB - neon

--- env.py
+++ env.py
@@ -2,5 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", 
-    "postgres://usosuytnklm:rYMuG0PUYEVB@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/five_salon_flock_826794"
+    "postgresql://vinadb_owner:PG5BoeTqkpj9@ep-billowing-dawn-a5a2zh5m.us-east-2.aws.neon.tech/vinadb?sslmode=require"
     )
