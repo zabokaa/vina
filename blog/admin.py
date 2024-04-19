c6ecff8b8376074188273fc8e9990bcbd3b886f9
zabokaa <sab_fem@gmx.de>
2024-04-19 22:15:25 +0200

    update blog app with summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Post
 from .models import Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ("content",)
+    list_display = ("title", "slug", "author", "created_on", "status")
+    search_fields = ["title"]
+    list_filter = ["status"]
+    prepopulated_fields = {"slug": ("title",)}
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
