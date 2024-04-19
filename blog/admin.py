02894044605a8cbf43cf7edcd975ec21fafa2a41
zabokaa <sab_fem@gmx.de>
2024-04-19 23:19:21 +0200

    install + wire up whitenoise + collect staticfiles

--- blog/admin.py
+++ blog/admin.py
@@ -6,11 +6,12 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ("content",)
+    
     list_display = ("title", "slug", "author", "created_on", "status")
     search_fields = ["title"]
     list_filter = ["status"]
     prepopulated_fields = {"slug": ("title",)}
+    summernote_fields = ("content",)
 
 # Register your models here.
 admin.site.register(Comment)
--- vinopro/settings.py
+++ vinopro/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".herokuapp.com",
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
