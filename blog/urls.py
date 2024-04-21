4bd93560c46da7464a0d09f91f486b86480ae763
zabokaa <sab_fem@gmx.de>
2024-04-21 03:59:31 +0200

    add post_detail VIEW

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
-    # urlpattern slug:slug creates a url path of the domain path plus the slug value:
-    # path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
 
