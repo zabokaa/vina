09e23f8d095342915b344265a6474798d08ccb00
zabokaa <sab_fem@gmx.de>
2024-04-21 05:32:42 +0200

    create blog/forms.py

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Comment
+
+
+
+class CommentForm(forms.ModelForm):
+    """
+        A form for creating and updating Comment instances.
+        The form includes only the 'body' field from the Comment model.
+    """
+    class Meta:
+        model = Comment
+        fields = ('body',)
