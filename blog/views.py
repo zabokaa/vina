0b42fda2623de11198682ea3eb7148c36c8ea341
zabokaa <sab_fem@gmx.de>
2024-04-21 10:36:25 +0200

    add django message to unlike/like button

--- blog/views.py
+++ blog/views.py
@@ -99,6 +99,8 @@ def like_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     if request.user in post.likes.all():
         post.likes.remove(request.user)
+        messages.success(request, 'You have unliked this post. It will be removed from your Wine Cellar.')
     else:
         post.likes.add(request.user)
+        messages.success(request, 'You have liked this post. It will be added to your Wine Cellar.')
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
