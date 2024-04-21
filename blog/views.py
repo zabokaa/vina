8b25a70d016ef51874e9a1f09069c5bebded37bc
zabokaa <sab_fem@gmx.de>
2024-04-21 05:33:59 +0200

    add comment_form @ blog/views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here:
 
@@ -28,10 +29,12 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
     return render(
         request,
         "blog/post_detail.html",
         {"post": post,
          "comments": comments,
-         "comment_count": comment_count,},
+         "comment_count": comment_count,
+         "comment_form": comment_form,},
     )
