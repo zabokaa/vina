d295e995f45e1c9632e5018dfd9703e71cf496a8
zabokaa <sab_fem@gmx.de>
2024-04-23 02:23:18 +0200

    format blog/views analo python linter

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,13 @@ from .models import Post, Comment
 from .forms import CommentForm
 
 
-# Create your views here:
+# views
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 4
+
 
 def post_detail(request, slug):
     """
@@ -32,7 +33,6 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-  
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -52,8 +52,9 @@ def post_detail(request, slug):
         {"post": post,
          "comments": comments,
          "comment_count": comment_count,
-         "comment_form": comment_form,}
+         "comment_form": comment_form, }
     )
+
 
 def comment_edit(request, slug, comment_id):
     """
@@ -78,6 +79,7 @@ def comment_edit(request, slug, comment_id):
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
+
 def comment_delete(request, slug, comment_id):
     """
      Display delete button for your own comments.
@@ -90,18 +92,27 @@ def comment_delete(request, slug, comment_id):
         comment.delete()
         messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
     else:
-        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'You can only delete your own comments!'
+            )
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
+
 @login_required
 def like_post(request, post_id):
+    """
+    Add or remove a post from the user's wine list (liked posts)."""
     post = get_object_or_404(Post, id=post_id)
     if request.method == 'POST':
         if request.user in post.likes.all():
             post.likes.remove(request.user)
-            messages.success(request, 'You have unliked this post. It will be removed from your Wine Cellar.')
+            messages.success(request, 'You have unliked this post. '
+                             'It will be removed from your Wine Cellar.')
         else:
             post.likes.add(request.user)
-            messages.success(request, 'You have liked this post. It will be added to your Wine Cellar.')
+            messages.success(request, 'You have liked this post. '
+                             'It will be added to your Wine Cellar.')
     return HttpResponseRedirect(reverse('post_detail', args=[post.slug]))
