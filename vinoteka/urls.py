5f3e887453b73e80c042a0197fab6b450ff81eb1
zabokaa <sab_fem@gmx.de>
2024-04-23 06:45:20 +0200

    create diary_list + diary_detail templates

--- vinoteka/urls.py
+++ vinoteka/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('diaries/new/', views.create_diary, name='create_diary'),
+    path('diaries/<int:pk>/', views.diary_detail, name='diary_detail'),
+    path('diaries/', views.diary_list, name='diary_list'),
     path('', views.liked_posts, name='vinoteka'),
 ]
--- vinoteka/views.py
+++ vinoteka/views.py
@@ -1,14 +1,23 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from blog.models import Post
 from .models import Diary
 from .forms import DiaryForm
 
+def diary_detail(request, pk):
+    diary = get_object_or_404(Diary, pk=pk)
+    return render(request, 'vinoteka/diary_detail.html', {'diary': diary})
+
+def diary_list(request):
+    diaries = Diary.objects.filter(user=request.user)
+    return render(request, 'vinoteka/diary_list.html', {'diaries': diaries})
 
 @login_required
 def liked_posts(request):
     posts = Post.objects.filter(likes=request.user)
-    return render(request, 'vinoteka/liked_posts.html', {'posts': posts})
+    form = DiaryForm()  # create a DiaryForm instance and add form to the context
+    diaries = Diary.objects.filter(user=request.user)
+    return render(request, 'vinoteka/liked_posts.html', {'posts': posts, 'form': form, 'diaries': diaries})
 
 
 @login_required
@@ -23,4 +32,4 @@ def create_diary(request):
             return redirect('diary_detail', pk=diary.pk)
     else:
         form = DiaryForm()
-    return render(request, 'vinoteka/create_diary.html', {'form': form, 'diaries': diaries})
+    return render(request, 'vinoteka/create_diary.html', {'form': form})
