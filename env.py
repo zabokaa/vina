3051fd471bb5e6e7e2fb68116d33518c181d5825
zabokaa <sab_fem@gmx.de>
2024-04-25 03:30:12 +0200

    install + wire-up cloudinary for storage users' diary pics upload

--- env.py
+++ env.py
@@ -5,3 +5,4 @@ os.environ.setdefault(
     "postgresql://vinadb_owner:PG5BoeTqkpj9@ep-billowing-dawn-a5a2zh5m.us-east-2.aws.neon.tech/vinadb?sslmode=require"
     )
 os.environ.setdefault("SECRET_KEY", "u#x$^C4pxz6sqVNs^M8DuQRHDM7!fz")
+os.environ.setdefault("CLOUDINARY_URL", "cloudinary://557819564883823:Jv5NTKghKCyv5vKP1QpPpgAqln0@dszgdroxd")
--- vinoteka/models.py
+++ vinoteka/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Model:
 class Diary(models.Model):
@@ -28,7 +29,7 @@ class Diary(models.Model):
     day = models.DateField()
     occasion = models.CharField(max_length=200)
     rating = models.IntegerField()
-    pic = models.ImageField(upload_to='diary_pics/', blank=True, null=True)
+    pic = CloudinaryField('image', blank=True, null=True)
     memory = models.TextField(blank=True, null=True)
     foodpairing = models.TextField(blank=True, null=True)
     again = models.BooleanField(default=False)
